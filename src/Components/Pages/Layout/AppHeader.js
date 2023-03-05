c66d147ef9932bc5ad92e77debcbc3c6daee5e44
BuildTools <unconfigured@null.spigotmc.org>
2023-03-06 01:07:15 +0200

    updated image

--- src/Components/Pages/Layout/AppHeader.js
+++ src/Components/Pages/Layout/AppHeader.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Nav from 'react-bootstrap/Nav';
-
+import image from '../../../Style/Images/bee.png';
 export default function AppHeader() {
   return (
     <>
       <Nav className="justify-content-center" activeKey="/home">
         <Nav.Item>
-          <Nav.Link href="/"><img style={{width: "150px", height: "150px"}} src="/images/bee.png"/></Nav.Link>
+          <Nav.Link href="/"><img style={{width: "150px", height: "150px"}} src={image}/></Nav.Link>
         </Nav.Item>
       </Nav>
     </>
